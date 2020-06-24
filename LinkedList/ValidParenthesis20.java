11fe3cf1049621fd85ca05daf36646c1358a5719
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-06-24 10:28:33 -0700

    20

---
+++ LinkedList/ValidParenthesis20.java
@@ -0,0 +1,32 @@
+package LinkedList;
+
+import java.util.Stack;
+
+public class ValidParenthesis20 {
+    class Solution {
+        public boolean isValid(String s) {
+            if (s == null || s.length() == 0) return true;
+            Stack<Character> brackets = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                char curr = s.charAt(i);
+                if (curr == '(' || curr == '{' || curr == '[') {
+                    brackets.push(curr);
+                } else {
+                    if (!brackets.isEmpty()) {
+                        char prev = brackets.pop();
+                        if (curr == ')' && prev != '(') {
+                            return false;
+                        } else if (curr == '}' && prev != '{') {
+                            return false;
+                        } else if (curr == ']' && prev != '[') {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            return brackets.isEmpty();
+        }
+    }
+}
