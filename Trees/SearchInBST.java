b24e7e6c339769a5d25a62ccbdb6b35b8e76e649
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-07-17 18:43:50 -0700

    700

---
+++ Trees/SearchInBST.java
@@ -0,0 +1,23 @@
+package Trees;
+
+import Misc.TreeNode;
+
+public class SearchInBST {
+    class Solution {
+        public TreeNode searchBST(TreeNode root, int val) {
+            return helper(root, val);
+        }
+
+        private TreeNode helper(TreeNode root, int val) {
+            if (root == null) return null;
+            if (root.val == val) {
+                return root;
+            }
+            if (root.val > val) {
+                return helper(root.left, val);
+            } else {
+                return helper(root.right, val);
+            }
+        }
+    }
+}
