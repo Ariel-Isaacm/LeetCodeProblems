46d88b2280957e8b6a8709dce42186a62eecccde
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-07-21 16:29:15 -0700

    961 better solution

--- Array/NRepeatedMatrix961.java
+++ Array/NRepeatedMatrix961.java
@@ -1,20 +1,13 @@
 package Array;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class NRepeatedMatrix961 {
     class Solution {
         public int repeatedNTimes(int[] A) {
-            Map<Integer, Integer> count = new HashMap();
-            for (int x: A) {
-                count.put(x, count.getOrDefault(x, 0) + 1);
+            int[] nums = new int[10000];
+            for(int i = 0; i< A.length; i++){
+                nums[A[i]]++;
+                if(nums[A[i]] == A.length/2) return A[i];
             }
-
-            for (int k: count.keySet())
-                if (count.get(k) > 1)
-                    return k;
-
             return -1;
         }
     }
