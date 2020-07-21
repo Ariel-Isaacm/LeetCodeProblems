6896b70a8d0dd0ccd3c1dd29965807ed375645c1
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-07-21 16:29:33 -0700

    format

--- Array/NRepeatedMatrix961.java
+++ Array/NRepeatedMatrix961.java
@@ -4,9 +4,9 @@ public class NRepeatedMatrix961 {
     class Solution {
         public int repeatedNTimes(int[] A) {
             int[] nums = new int[10000];
-            for(int i = 0; i< A.length; i++){
+            for (int i = 0; i < A.length; i++) {
                 nums[A[i]]++;
-                if(nums[A[i]] == A.length/2) return A[i];
+                if (nums[A[i]] == A.length / 2) return A[i];
             }
             return -1;
         }
