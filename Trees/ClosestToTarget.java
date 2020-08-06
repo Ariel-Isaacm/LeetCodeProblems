4449283dc2067431d6f8d68c7ffa517bed76bd02
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-08-06 00:10:58 -0700

    closest to range in a bst

---
+++ Trees/ClosestToTarget.java
@@ -0,0 +1,23 @@
+package Trees;
+
+import Misc.TreeNode;
+
+public class ClosestToTarget {
+    public int closestToTarget(TreeNode root, double target) {
+        return helper(root, target, Integer.MAX_VALUE);
+    }
+
+    private int helper(TreeNode node, double target, int closest) {
+        if (node != null) {
+            if (Math.abs(node.val - target) < Math.abs(closest - target)) {
+                closest = node.val;
+            }
+            if (node.val > target) {
+                closest = helper(node.left, target, closest);
+            } else {
+                closest = helper(node.right, target, closest);
+            }
+        }
+        return closest;
+    }
+}
