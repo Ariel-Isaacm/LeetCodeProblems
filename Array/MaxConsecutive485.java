5044d104166e9f479b05415628d23703c41149f5
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-08-19 16:25:05 -0700

    485

---
+++ Array/MaxConsecutive485.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class MaxConsecutive485 {
+    class Solution {
+        public int findMaxConsecutiveOnes(int[] nums) {
+            int max = -1;
+            int count = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == 1) {
+                    count++;
+                } else {
+                    if (count > max) {
+                        max = count;
+                    }
+                    count = 0;
+                }
+            }
+            return Math.max(count, max);
+        }
+    }
+}
