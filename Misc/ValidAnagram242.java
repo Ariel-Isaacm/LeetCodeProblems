7dc963065e497dcc3aff83407d243ee9cf9ede4c
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-08-24 12:31:51 -0700

    242

---
+++ Misc/ValidAnagram242.java
@@ -0,0 +1,21 @@
+package Misc;
+
+public class ValidAnagram242 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] sLetters = new int[26];
+        int[] tLetters = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            sLetters[s.charAt(i) - 'a']++;
+            tLetters[t.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < tLetters.length; i++) {
+            if (sLetters[i] != tLetters[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
