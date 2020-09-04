41de9fae82b62832e5081cfa0f646c06a2858fa6
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-09-04 14:43:32 -0700

    1389-comments

--- Array/TargetArrayInGivenOrder1389.java
+++ Array/TargetArrayInGivenOrder1389.java
@@ -7,6 +7,8 @@ public class TargetArrayInGivenOrder1389 {
     class Solution {
         public int[] createTargetArray(int[] nums, int[] index) {
             // probably not the best approach? do we want to sacrifice memory or time?
+            // todo not comfortable with this solution, linked list and play with the pointers as we insert the elements and then empty it in an
+            // todo array? or play with the array in-place and make it more time consuming?
             if (nums == null || nums.length < 1) return null;
             int[] ans = new int[nums.length];
             List<Integer> aux = new ArrayList<>();
