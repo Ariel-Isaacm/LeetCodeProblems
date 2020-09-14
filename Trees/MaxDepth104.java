901a7325305d0697c735ac37a50dc40185590be1
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-09-13 17:31:03 -0700

    104

---
+++ Trees/MaxDepth104.java
@@ -0,0 +1,40 @@
+package Trees;
+
+import Misc.TreeNode;
+
+import java.util.LinkedList;
+
+public class MaxDepth104 {
+
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            // leetcode's statistics showed that dfs was better in terms of speed (1ms vs 0ms)
+            if(root == null) return 0;
+            return bfs(root);
+            // return dfs(root, 0);
+        }
+        private int dfs(TreeNode node, int depth){
+            if(node == null) return depth;
+            depth++;
+            return Math.max(dfs(node.left, depth), dfs(node.right, depth));
+        }
+
+        private int bfs(TreeNode root){
+            LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+            queue.add(root);
+            int ans = 0;
+            while(!queue.isEmpty()){
+                int size = queue.size();
+                ans++;
+                while(size > 0){
+                    TreeNode node = queue.pop();
+                    if(node.left != null) queue.add(node.left);
+                    if(node.right != null) queue.add(node.right);
+                    size--;
+                }
+            }
+            return ans;
+
+        }
+    }
+}
