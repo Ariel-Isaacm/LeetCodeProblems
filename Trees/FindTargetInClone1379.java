1ce9e351e79b21d6db81ff218f655eeb317a4c68
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-09-25 16:13:52 -0700

    1379

---
+++ Trees/FindTargetInClone1379.java
@@ -0,0 +1,20 @@
+package Trees;
+
+import Misc.TreeNode;
+
+public class FindTargetInClone1379 {
+
+    class Solution {
+        public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+            if (original == null || cloned == null || target == null) return null;
+            return dfs(target, cloned);
+        }
+
+        private TreeNode dfs(TreeNode target, TreeNode node) {
+            if (node == null) return null;
+            if (node.val == target.val) return node;
+            TreeNode ans = dfs(target, node.left);
+            return ans == null ? dfs(target, node.right) : ans;
+        }
+    }
+}
