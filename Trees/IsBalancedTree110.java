e0efccf6a6ee6e66ff6ab0bde1994f026c69458f
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-09-29 10:28:39 -0700

    110

---
+++ Trees/IsBalancedTree110.java
@@ -0,0 +1,24 @@
+package Trees;
+
+import Misc.TreeNode;
+
+public class IsBalancedTree110 {
+    class Solution {
+        public boolean isBalanced(TreeNode root) {
+            if (root == null) return true;
+            int left = level(root.left);
+            int right = level(root.right);
+            if (Math.abs(left - right) > 1) {
+                return false;
+            }
+            return isBalanced(root.right) && isBalanced(root.left);
+        }
+
+        private int level(TreeNode node) {
+            if (node == null) return 0;
+            int left = level(node.left);
+            int right = level(node.right);
+            return Math.max(left, right) + 1;
+        }
+    }
+}
