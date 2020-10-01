0d92baedc26e0d8a48beabd67db3a1114adf8394
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-10-01 10:29:14 -0700

    933

---
+++ LinkedList/RecentCalls933.java
@@ -0,0 +1,27 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class RecentCalls933 {
+    class RecentCounter {
+        LinkedList<Integer> state;
+
+        public RecentCounter() {
+            state = new LinkedList<>();
+        }
+
+        public int ping(int t) {
+            state.addLast(t);
+            while (state.size() > 0) {
+                if (state.peek() < t - 3000) {
+                    state.poll();
+                } else {
+                    break;
+                }
+            }
+
+            return state.size();
+        }
+    }
+
+}
