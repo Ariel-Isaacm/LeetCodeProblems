366a6ad299fb54e83d11bd7f15d9b7e688f7dd5d
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-10-01 11:03:32 -0700

    111

---
+++ Trees/MinimumDepthBinaryTree111.java
@@ -0,0 +1,18 @@
+package Trees;
+
+import Misc.TreeNode;
+
+public class MinimumDepthBinaryTree111 {
+    class Solution {
+        public int minDepth(TreeNode root) {
+            if (root == null) return 0;
+            return min(root, 1);
+        }
+
+        private int min(TreeNode node, int level) {
+            if (node == null) return Integer.MAX_VALUE;
+            if (node.left == null && node.right == null) return level;
+            return Math.min(min(node.left, level + 1), min(node.right, level + 1));
+        }
+    }
+}
