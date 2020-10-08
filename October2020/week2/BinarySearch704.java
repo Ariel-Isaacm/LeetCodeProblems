c36736ff16d1b65a902aa06010b65b420128fef6
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-10-08 12:43:50 -0700

    704

---
+++ October2020/week2/BinarySearch704.java
@@ -0,0 +1,19 @@
+package October2020.week2;
+
+public class BinarySearch704 {
+    class Solution {
+        public int search(int[] nums, int target) {
+            int index, low = 0, high = nums.length - 1;
+            while (low <= high) {
+                index = low + (high - low) / 2;
+                if (nums[index] == target) return index;
+                if (target < nums[index]) {
+                    high = index - 1;
+                } else {
+                    low = index + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
