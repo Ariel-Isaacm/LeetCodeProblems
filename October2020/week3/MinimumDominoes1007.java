6ae973a3cb178a6d218e584234aede824a1d64f0
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-10-19 13:33:19 -0700

    1007

---
+++ October2020/week3/MinimumDominoes1007.java
@@ -0,0 +1,37 @@
+package October2020.week3;
+
+import java.util.HashSet;
+
+public class MinimumDominoes1007 {
+    class Solution {
+        public int minDominoRotations(int[] A, int[] B) {
+            HashSet<Integer> aSet = new HashSet<>();
+            HashSet<Integer> bSet = new HashSet<>();
+            for (int i = 0; i < A.length; i++) {
+                aSet.add(A[i]);
+                bSet.add(B[i]);
+            }
+            return Math.min(minDomino(A, B, aSet), minDomino(B, A, bSet));
+        }
+
+        private int minDomino(int[] A, int[] B, HashSet<Integer> set) {
+            int ans = Integer.MAX_VALUE;
+            for (int curr : set) {
+                int min = 0;
+                for (int i = 0; i < A.length; i++) {
+                    if (A[i] != curr) {
+                        if (B[i] == curr) {
+                            min++;
+                        } else {
+                            min = Integer.MAX_VALUE;
+                            break; //  can't do the swap;
+                        }
+
+                    }
+                }
+                ans = Math.min(ans, min);
+            }
+            return ans == Integer.MAX_VALUE ? -1 : ans;
+        }
+    }
+}
