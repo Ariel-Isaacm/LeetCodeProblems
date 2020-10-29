ae811cf0862cde426c7397f20b977cb0c68f9f93
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-10-29 01:34:45 -0700

    849 + insomnia

---
+++ October2020/week5/MaximizeDistanceClosestPerson849.java
@@ -0,0 +1,32 @@
+package October2020.week5;
+
+public class MaximizeDistanceClosestPerson849 {
+    public int maxDistToClosest(int[] seats) {
+        int max = Integer.MIN_VALUE;
+        int anchor = 0;
+        // left
+        if (seats[0] == 0) {
+            for (int i = 1; i < seats.length; i++) {
+                if (seats[i] == 1) {
+                    max = i;
+                    anchor = i;
+                    break;
+                }
+            }
+        }
+
+        // middle
+        for (int i = anchor + 1; i < seats.length; i++) {
+            if (seats[i] == 1) {
+                max = Math.max(max, (i - anchor) / 2);
+                anchor = i;
+            }
+        }
+
+        //right
+        if (seats[seats.length - 1] == 0) {
+            max = Math.max(max, ((seats.length - 1) - anchor));
+        }
+        return max;
+    }
+}
