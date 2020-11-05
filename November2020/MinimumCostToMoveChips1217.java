a3f5abe60b132612cc0a43f996feba0f736af7d7
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-11-05 14:25:43 -0700

    1217

---
+++ November2020/MinimumCostToMoveChips1217.java
@@ -0,0 +1,18 @@
+package November2020;
+
+public class MinimumCostToMoveChips1217 {
+    class Solution {
+        public int minCostToMoveChips(int[] position) {
+            int even_cnt = 0;
+            int odd_cnt = 0;
+            for (int i : position) {
+                if (i % 2 == 0) {
+                    even_cnt++;
+                } else {
+                    odd_cnt++;
+                }
+            }
+            return Math.min(odd_cnt, even_cnt);
+        }
+    }
+}
