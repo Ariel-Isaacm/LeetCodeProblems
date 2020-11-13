abf6687225f842729072cf6e805b52141fbb59b8
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-11-13 15:38:36 -0700

    117

--- November2020/PopulatingNextPointer117.java
+++ November2020/PopulatingNextPointer117.java
@@ -1,4 +1,4 @@
-package Trees;
+package November2020;
 
 import java.util.LinkedList;
 
@@ -45,6 +45,5 @@ public class PopulatingNextPointer117 {
             }
             return root;
         }
-
     }
 }
