f25a9e44b792551069d7f96be3bf6cf8f6a2b025
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-11-14 18:23:55 -0700

    458

---
+++ Misc/PoorPigs458.java
@@ -0,0 +1,17 @@
+package Misc;
+
+public class PoorPigs458 {
+    class Solution {
+        public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+            int time = (minutesToTest / minutesToDie) + 1;
+            int pigs = 0;
+            int total = 1;
+
+            while (total < buckets) {
+                ++pigs;
+                total *= time;
+            }
+            return pigs;
+        }
+    }
+}
