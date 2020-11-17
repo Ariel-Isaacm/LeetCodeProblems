a52eeada8368fa4b184613ee614b871a3ab47ac2
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-11-17 15:26:10 -0700

    1051

---
+++ November2020/HeightChecker1051.java
@@ -0,0 +1,20 @@
+package November2020;
+
+import java.util.Arrays;
+
+public class HeightChecker1051 {
+    class Solution {
+        public int heightChecker(int[] heights) {
+            int copy[] = heights.clone();
+            Arrays.sort(copy);
+            int height = 0;
+
+            for (int i = 0; i < copy.length; i++) {
+                if (copy[i] != heights[i]) {
+                    height++;
+                }
+            }
+            return height;
+        }
+    }
+}
