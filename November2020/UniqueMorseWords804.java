e7ebddb919026104bb0519819f821b6d63b3536b
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-11-22 20:18:25 -0700

    804

---
+++ November2020/UniqueMorseWords804.java
@@ -0,0 +1,25 @@
+package November2020;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueMorseWords804 {
+    class Solution {
+        public int uniqueMorseRepresentations(String[] words) {
+            String[] MORSE = new String[]{".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+                    "....", "..", ".---", "-.-", ".-..", "--", "-.",
+                    "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+                    "...-", ".--", "-..-", "-.--", "--.."};
+
+            Set<String> seen = new HashSet<String>();
+            for (String word : words) {
+                StringBuilder code = new StringBuilder();
+                for (char c : word.toCharArray())
+                    code.append(MORSE[c - 'a']);
+                seen.add(code.toString());
+            }
+
+            return seen.size();
+        }
+    }
+}
