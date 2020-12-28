5a0e9383a7c00490cf479ee62e2c5f21d9a232ef
Ariel-Isaacm <ariel.isaacm@gmail.com>
2020-12-28 15:35:07 -0700

    745

---
+++ year2020/December2020/ReachANumber745.java
@@ -0,0 +1,14 @@
+package year2020.December2020;
+
+public class ReachANumber745 {
+    class Solution {
+        public int reachNumber(int target) {
+            target = Math.abs(target);
+            int k = 0;
+            while (target > 0)
+                target -= ++k;
+            return target % 2 == 0 ? k : k + 1 + k % 2;
+
+        }
+    }
+}
