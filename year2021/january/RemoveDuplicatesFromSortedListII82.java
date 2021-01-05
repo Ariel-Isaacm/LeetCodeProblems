7a01bd1cb72dad1a022b88e1cd29fd671fd5504d
Ariel-Isaacm <ariel.isaacm@gmail.com>
2021-01-05 12:52:48 -0700

    82

---
+++ year2021/january/RemoveDuplicatesFromSortedListII82.java
@@ -0,0 +1,27 @@
+package year2021.january;
+
+import Misc.ListNode;
+
+public class RemoveDuplicatesFromSortedListII82 {
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            ListNode sentinel = new ListNode(0, head);
+
+            ListNode pred = sentinel;
+
+            while (head != null) {
+                if (head.next != null && head.val == head.next.val) {
+                    while (head.next != null && head.val == head.next.val) {
+                        head = head.next;
+                    }
+                    pred.next = head.next;
+                } else {
+                    pred = pred.next;
+                }
+
+                head = head.next;
+            }
+            return sentinel.next;
+        }
+    }
+}
