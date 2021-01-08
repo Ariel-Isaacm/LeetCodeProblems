2f106d90b3f482ce123da4deb22c96f652a0d353
Ariel-Isaacm <ariel.isaacm@gmail.com>
2021-01-07 18:03:11 -0700

    4

---
+++ year2021/january/LongestUniqueSequence4.java
@@ -0,0 +1,25 @@
+package year2021.january;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestUniqueSequence4 {
+    class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            int r = 0;
+            int l = 0;
+            int res = 0;
+            Set<Character> set = new HashSet<>();
+            while (r < s.length()) {
+                if (set.contains(s.charAt(r))) {
+                    set.remove(s.charAt(l++));
+                } else {
+                    set.add(s.charAt(r++));
+                    res = Math.max(res, set.size());
+                }
+            }
+
+            return res;
+        }
+    }
+}
